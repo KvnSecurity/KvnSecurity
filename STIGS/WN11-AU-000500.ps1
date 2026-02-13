327702ddf4911131099ebc67e0cdc24b4bfd92fe
Kavan <kavan.singh98@yahoo.com>
2026-02-12 19:24:41 -0500

    Update WN11-AU-000500.ps1

--- STIGS/WN11-AU-000500.ps1
+++ STIGS/WN11-AU-000500.ps1
@@ -13,16 +13,11 @@
     Plugin IDs      : N/A
     STIG-ID         : WN11-AU-000500
 
-.TESTED ON
-    Date(s) Tested  : 
-    Tested By       : 
-    Systems Tested  : 
-    PowerShell Ver. : 
 
 .USAGE
     Put any usage instructions here.
     Example syntax:
-    PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
+    PS C:\> .\__remediation_template(STIG-ID-WN11-AU-000500).ps1 
 #>
 
 # Define variables
